7d011a2647cbf7106a1dad459085e1cb989d68d1
Josh Maggard <jmaggard62@gmail.com>
2021-04-24 22:12:37 -0500

    Fish death anim

--- Assets/Objects/Fish.cs
+++ Assets/Objects/Fish.cs
@@ -4,6 +4,8 @@ public class Fish : DisplaceObject
 {
     public override void Displace(Vector3 dir)
     {
-        Destroy(gameObject);
+        GetComponent<Animator>().SetTrigger("Die");
+        GetComponent<BoxCollider2D>().enabled = false;
+        Destroy(gameObject, 2f);
     }
 }
