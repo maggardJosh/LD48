f0ca4417ca924dda302dcc5479e320ce091cf066
Josh Maggard <jmaggard62@gmail.com>
2021-04-25 17:49:31 -0500

    bubble particles

---
+++ Assets/BubbleSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class BubbleSpawner : MonoBehaviour
+{
+    public float minX = -1f;
+    public float maxX = 1f;
+
+    [Range(0,1)]
+    public float randomChance = .2f;
+    public float randomTimeCheck = .1f;
+
+    public GameObject[] bubblePrefabs;
+
+    private float count = 0;
+    void Update()
+    {
+        if (randomTimeCheck <= 0)
+            return;
+        count += Time.deltaTime;
+        while (count > randomTimeCheck)
+        {
+            count -= 1;
+            count -= randomTimeCheck;
+            if (Random.Range(0f, 1) <= randomChance)
+            {
+                float randomXPos = Random.Range(minX, maxX);
+                int prefabInd = Random.Range(0, bubblePrefabs.Length);
+                Instantiate(bubblePrefabs[prefabInd], transform.position + Vector3.right * randomXPos, Quaternion.identity);
+            }
+        }
+    }
+}
--- Assets/RandomFloatStartTime.cs
+++ Assets/RandomFloatStartTime.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class RandomFloatStartTime : MonoBehaviour
 {
+    public float minSpeed = .5f;
+    public float maxSpeed = 1f;
+
+    public float minWeight = .5f;
+    public float maxWeight = 1f;
+    
     void Start()
     {
         var animator = GetComponent<Animator>();
         animator.Play("Float", animator.GetLayerIndex("Float"), Random.Range(0, 1f) );
-        animator.SetLayerWeight(animator.GetLayerIndex("Float"), Random.Range(.5f, 1f));
-        animator.SetFloat("FloatSpeedMultiplier", Random.Range(.5f,1f));
+        animator.SetLayerWeight(animator.GetLayerIndex("Float"), Random.Range(minWeight, maxWeight));
+        animator.SetFloat("FloatSpeedMultiplier", Random.Range(minSpeed, maxSpeed));
     }
 
     // Update is called once per frame
