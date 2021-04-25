5a7fed89d1a9f2f9c6919c36deaf93ca2fd496c9
Josh Maggard <jmaggard62@gmail.com>
2021-04-24 20:39:45 -0500

    oops, fix build again

--- Assets/TridentAnimation.cs
+++ Assets/TridentAnimation.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class TridentAnimation : MonoBehaviour
 {
     private float _count = 0;
-    
-    public void AnimateTo(Vector3)
+
+    public void AnimateTo(Vector3 pos)
+    {
+        
+    }
 }
