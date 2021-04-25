7dc18c2beba21d467fa8b2456ecbe9bfb7c58b30
Josh Maggard <jmaggard62@gmail.com>
2021-04-24 20:38:42 -0500

    fix web build

---
+++ Assets/TridentAnimation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TridentAnimation : MonoBehaviour
+{
+    private float _count = 0;
+    
+    public void AnimateTo(Vector3)
+}
