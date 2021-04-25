8bded504830bdd3e877b4aa031914cad5716648d
Josh Maggard <jmaggard62@gmail.com>
2021-04-24 23:12:58 -0500

    polish float anim for several objects

---
+++ Assets/RandomFloatStartTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomFloatStartTime : MonoBehaviour
+{
+    void Start()
+    {
+        var animator = GetComponent<Animator>();
+        animator.Play("Float", animator.GetLayerIndex("Float"), Random.Range(0, 1f) );
+        animator.SetLayerWeight(animator.GetLayerIndex("Float"), Random.Range(.5f, 1f));
+        animator.SetFloat("FloatSpeedMultiplier", Random.Range(.5f,1f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
